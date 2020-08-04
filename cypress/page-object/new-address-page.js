261179cff2f5842d898bb2844afc1fa595d2fd62
Jeevan.Adiga <adiga.jeevan@gmail.com>
2020-08-04 22:18:36 +0530

    add new fields

--- cypress/page-object/new-address-page.js
+++ cypress/page-object/new-address-page.js
@@ -5,5 +5,9 @@ export function createAddress() {
     cy.get('#address_first_name').type(faker.name.firstName());
     cy.get('#address_last_name').type(faker.name.lastName());
 
+    cy.get('#address_street_address').type(faker.address.streetAddress())
+    cy.get('#address_city').type(faker.address.city())
+    cy.get('#address_zip_code').type(faker.address.zipCode().split('-')[0])
+
     cy.get('[data-test=submit]').click();
 }
